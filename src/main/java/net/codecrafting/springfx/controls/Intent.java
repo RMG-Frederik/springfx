40e62db4f37e43756cd42588cfe8d945b51412a8
lucasmarotta <lucas.marotta@codecrafting.net>
2018-02-20 16:24:57 -0300

    Added Intent tests and javadoc

--- src/main/java/net/codecrafting/springfx/controls/Intent.java
+++ src/main/java/net/codecrafting/springfx/controls/Intent.java
@@ -1,20 +1,53 @@
 package net.codecrafting.springfx.controls;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The transaction data class that is used to switch {@link ViewContext}s. The idea
+ * is inspired by Android Intent class. This class provide some context to the 
+ * JavaFX controllers, so that the user could pass some parameters to the new controller
+ * or get a reference to the previous controller with the {@link #callerContext}.
+ * 
+ * @author Lucas Marotta
+ * @see #getCallerContext()
+ * @see #getViewClass()
+ * @see #getExtra(String)
+ * @see #putExtra(String, Object)
+ * @see #clearExtra()
+ */
 public class Intent 
 {
+	/**
+	 * The caller context or the previous controller that is calling for a new {@link ViewContext}
+	 */
 	private ViewContext callerContext;
-	private Class<? extends ViewContext> viewClass;
-	private Map<String, Serializable> extraData;
 	
+	/**
+	 * The {@link ViewContext} JavaFX controller class that will be loaded
+	 */
+	private Class<? extends ViewContext> viewClass;
+	
+	/**
+	 * Map to hold the parameters of the transaction to the new {@link ViewContext}
+	 */
+	private Map<String, Object> extraData;
+	
+	/**
+	 * Create a new instance of {@link Intent}. The extra data will be initialized
+	 * with empty parameters.
+	 * 
+	 * <br><b>NOTE:</b> The callerContext and the viewClass can be {@literal null}.
+	 * 
+	 * @param callerContext the previous controller that is calling. Use the instance (this) to
+	 * pass the current controller.
+	 * @param viewClass the new {@link ViewContext} JavaFX controller class to be loaded
+	 */
 	public Intent(ViewContext callerContext, Class<? extends ViewContext> viewClass)
 	{
 		this.callerContext = callerContext;
 		this.viewClass = viewClass;
-		extraData = new HashMap<String, Serializable>();
+		extraData = new HashMap<String, Object>();
 	}
 
 	public ViewContext getCallerContext() 
@@ -27,16 +60,39 @@ public class Intent
 		return viewClass;
 	}
 	
-	public void putExtra(String field, Serializable value)
+	/**
+	 * Set a extra parameter to be accessible for the new controller to be loaded.
+	 * @param field the name reference to find the parameter
+	 * @param value the value of the name reference
+	 * @throws IllegalArgumentException if field is null
+	 * @throws IllegalArgumentException if field is empty
+	 */
+	public void putExtra(String field, Object value)
 	{
-		extraData.put(field, value);
+		if(field != null) {
+			if(!field.isEmpty()) {
+				extraData.put(field, value);	
+			} else {
+				throw new IllegalArgumentException("field must not be empty");
+			}
+		} else {
+			throw new IllegalArgumentException("field must not be null");
+		}
 	}
 	
-	public Serializable getExtra(String field)
+	/**
+	 * Find the extra parameter that match the field
+	 * @param field he name reference to find the parameter
+	 * @return the value of the name reference
+	 */
+	public Object getExtra(String field)
 	{
 		return extraData.get(field);
 	}
 	
+	/**
+	 * Clear all parameters previously added
+	 */
 	public void clearExtra()
 	{
 		extraData.clear();
---
+++ src/test/java/net/codecrafting/springfx/controls/IntentTest.java
@@ -0,0 +1,91 @@
+package net.codecrafting.springfx.controls;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mockito;
+
+import net.codecrafting.springfx.application.controllers.TestController;
+
+public class IntentTest 
+{
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void instantiation()
+	{
+		Intent intent = new Intent(null, null);
+		assertNull(intent.getCallerContext());
+		assertNull(intent.getViewClass());
+		ViewContext mockContext = Mockito.mock(ViewContext.class);
+		intent = new Intent(mockContext, TestController.class);
+		assertNotNull(intent.getCallerContext());
+		assertNotNull(intent.getViewClass());
+		assertNull(intent.getExtra("test"));
+	}
+	
+	@Test
+	public void getExtra()
+	{
+		ViewContext mockContext = Mockito.mock(ViewContext.class);
+		Intent intent = new Intent(mockContext, TestController.class);
+		assertNull(intent.getExtra("test"));
+		intent.putExtra("test", new Integer(1));
+		assertEquals(new Integer(1), intent.getExtra("test"));
+	}
+	
+	@Test
+	public void putMultipleExtras()
+	{
+		ViewContext mockContext = Mockito.mock(ViewContext.class);
+		Intent intent = new Intent(mockContext, TestController.class);
+		intent.putExtra("test", new Integer(1));
+		intent.putExtra("test", "bananas");
+		intent.putExtra("test2", "apple");
+		intent.putExtra("test3", null);
+		assertEquals("bananas", intent.getExtra("test"));
+		assertEquals("apple", intent.getExtra("test2"));
+		assertNull(intent.getExtra("test3"));
+	}
+	
+	@Test
+	public void putExtraFieldMustNotBeNull()
+	{
+		this.thrown.expect(IllegalArgumentException.class);
+		this.thrown.expectMessage("field must not be null");
+		ViewContext mockContext = Mockito.mock(ViewContext.class);
+		Intent intent = new Intent(mockContext, TestController.class);
+		intent.putExtra(null, null);
+	}
+	
+	@Test
+	public void putExtraFieldMustNotBeEmpty()
+	{
+		this.thrown.expect(IllegalArgumentException.class);
+		this.thrown.expectMessage("field must not be empty");
+		ViewContext mockContext = Mockito.mock(ViewContext.class);
+		Intent intent = new Intent(mockContext, TestController.class);
+		intent.putExtra("", null);
+	}
+	
+	@Test
+	public void clearExtra()
+	{
+		ViewContext mockContext = Mockito.mock(ViewContext.class);
+		Intent intent = new Intent(mockContext, TestController.class);
+		intent.clearExtra();
+		assertNull(intent.getExtra("test"));
+		intent.putExtra("test", "banana");
+		intent.putExtra("test2", "apple");
+		intent.clearExtra();
+		assertNull(intent.getExtra("test"));
+		assertNull(intent.getExtra("test2"));
+		intent.putExtra("test", "apple");
+		assertEquals("apple", intent.getExtra("test"));
+	}
+}
