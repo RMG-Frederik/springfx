8bac8f2d2298460422b7f19ca16fe07c33be0060
lucasmarotta <lucas.marotta@codecrafting.net>
2018-02-23 03:28:42 -0300

    Added tests and javadoc for StageContext class

--- src/main/java/net/codecrafting/springfx/controls/StageContext.java
+++ src/main/java/net/codecrafting/springfx/controls/StageContext.java
@@ -1,31 +1,88 @@
 package net.codecrafting.springfx.controls;
 
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import net.codecrafting.springfx.annotation.ViewController;
 
+/**
+ * This class is used to abstract the root JavaFX controller. The goal of {@link StageContext} is
+ * to provide a standard abstraction of the root JavaFX controller for the {@link ViewStage}. This class
+ * is also responsible for swapping the main node.
+ * 
+ * @author Lucas Marotta
+ * @see #setViewStageTitle(String)
+ * @see #getMainNode()
+ * @see #swapContent(ViewContext)
+ */
 public abstract class StageContext extends ViewContext
 {	
+	/**
+	 * Create a new instance of a abstraction of {@link StageContext}.
+	 * Use this constructor to set {@link #viewName} and {@link #viewTitle} by
+	 * the controller name or via {@link ViewController} annotation
+	 */
+	public StageContext()
+	{
+		super();
+	}
+	
+	/**
+	 * Create a new instance of a abstraction of {@link StageContext}.
+	 * This constructor allow manual setup for viewName and viewTitle.
+	 * @param viewName the name of the view that will be used for find a corresponding FXML file
+	 * @param viewTitle the name that can be use for a title of this view context
+	 */
+	public StageContext(String viewName, String viewTitle)
+	{
+		super(viewName, viewTitle);
+	}
+	
+	/**
+	 * Method that can perform the configuration of the {@link ViewContext#viewTitle} to the interface
+	 * The {@link ViewStage} will call this method passing the {@link ViewContext#viewTitle} from the
+	 * loaded {@link ViewContext} {@link Intent}. Use this to set a title for your current view.
+	 * @param title the {@link ViewContext#viewTitle} to be set as title on the interface.
+	 */
 	public abstract void setViewStageTitle(String title);
 	
+	/**
+	 * Get the root main {@link AnchorPane} node for this view context. The main node for the {@link StageContext}
+	 * needs to be a {@link AnchorPane} in order to perform a correct layout positioning for internal nodes.
+	 * Your {@link Scene} root node node don't need to be this main node, but you need to put a {@link AnchorPane} 
+	 * inside that node. Need to be implemented.
+	 * @return the root main {@link AnchorPane} node of this stage context.
+	 */
 	@Override
 	public abstract AnchorPane getMainNode();
 	
+	/**
+	 * Swap the stage context main node first child (if present) to the new {@link ViewContext} loaded main node.
+	 * This method will call for {@link ViewContext#swapAnimation(Node)} to perform the node animation.
+	 * @param viewController the {@link ViewContext} containing the main node to be swapped.
+	 * @throws IllegalArgumentException if the viewController is null
+	 * @throws NullPointerException if the {@link StageContext} and {@link ViewContext} mainNode is null
+	 */
 	public void swapContent(final ViewContext viewController)
 	{
-		final AnchorPane stageContextNode = getMainNode();
-		final Node viewNode = viewController.getMainNode();
-		if(stageContextNode != null && viewNode != null) {
-			viewNode.setVisible(true);
-			viewNode.autosize();
-			if(stageContextNode.getChildren().size() > 0) stageContextNode.getChildren().remove(0);
-			stageContextNode.getChildren().add(0, viewNode);
-			AnchorPane.setTopAnchor(viewNode, 0.0);
-			AnchorPane.setBottomAnchor(viewNode, 0.0);
-			AnchorPane.setLeftAnchor(viewNode, 0.0);
-			AnchorPane.setRightAnchor(viewNode, 0.0);
-			viewController.swapAnimation(viewNode);
+		if(viewController != null) {
+			final AnchorPane stageContextNode = getMainNode();
+			final Node viewNode = viewController.getMainNode();
+			if(stageContextNode != null && viewNode != null) {
+				viewNode.setVisible(true);
+				viewNode.autosize();
+				if(stageContextNode.getChildren().size() > 0) stageContextNode.getChildren().remove(0);
+				stageContextNode.getChildren().add(0, viewNode);
+				AnchorPane.setTopAnchor(viewNode, 0.0);
+				AnchorPane.setBottomAnchor(viewNode, 0.0);
+				AnchorPane.setLeftAnchor(viewNode, 0.0);
+				AnchorPane.setRightAnchor(viewNode, 0.0);
+				viewController.swapAnimation(viewNode);
+			} else {
+				throw new NullPointerException("StageContext mainNode and content must not be null");
+			}	
 		} else {
-			throw new NullPointerException("StageContext mainNode and content must exist");
+			throw new IllegalArgumentException("viewController must not be null");
 		}
 	}
 }
---
+++ src/test/java/net/codecrafting/springfx/controls/StageContextTest.java
@@ -0,0 +1,172 @@
+package net.codecrafting.springfx.controls;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+
+public class StageContextTest 
+{
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void swapContentBadArgument()
+	{
+		this.thrown.expect(IllegalArgumentException.class);
+		this.thrown.expectMessage("viewController must not be null");
+		StageContext context = new StageContext("test", "test title") {
+			
+			@Override
+			protected void onStart() {}
+			
+			@Override
+			protected void onCreate(URL location, ResourceBundle resources) {}
+			
+			@Override
+			public void setViewStageTitle(String title) {}
+			
+			@Override
+			public AnchorPane getMainNode() {return null;}
+		};
+		context.swapContent(null);
+	}
+	
+	@Test
+	public void swapContentBadContent()
+	{
+		StageContext context = new StageContext("test", "test title") {
+			
+			@Override
+			protected void onStart() {}
+			
+			@Override
+			protected void onCreate(URL location, ResourceBundle resources) {}
+			
+			@Override
+			public void setViewStageTitle(String title) {}
+			
+			@Override
+			public AnchorPane getMainNode() {return null;}
+		};
+		ViewContext viewController = Mockito.mock(ViewContext.class);
+		Mockito.when(viewController.getMainNode()).thenReturn(null);
+		
+		try {
+			context.swapContent(viewController);
+			assertFalse("NullPointerException not thrown", true);
+		} catch(Exception e) {
+			assertEquals(NullPointerException.class, e.getClass());
+			assertEquals("StageContext mainNode and content must not be null", e.getMessage());
+		}
+		
+		context = new StageContext("test", "test title") {
+			
+			@Override
+			protected void onStart() {}
+			
+			@Override
+			protected void onCreate(URL location, ResourceBundle resources) {}
+			
+			@Override
+			public void setViewStageTitle(String title) {}
+			
+			@Override
+			public AnchorPane getMainNode() {return null;}
+		};
+		Mockito.reset(viewController);
+		Mockito.when(viewController.getMainNode()).thenReturn(new AnchorPane());
+		
+		try {
+			context.swapContent(viewController);
+			assertFalse("NullPointerException not thrown", true);
+		} catch(Exception e) {
+			assertEquals(NullPointerException.class, e.getClass());
+			assertEquals("StageContext mainNode and content must not be null", e.getMessage());
+		}
+		
+		context = new StageContext("test", "test title") {
+			
+			@Override
+			protected void onStart() {}
+			
+			@Override
+			protected void onCreate(URL location, ResourceBundle resources) {}
+			
+			@Override
+			public void setViewStageTitle(String title) {}
+			
+			@Override
+			public AnchorPane getMainNode() {return new AnchorPane();}
+		};
+		Mockito.reset(viewController);
+		Mockito.when(viewController.getMainNode()).thenReturn(null);
+		
+		try {
+			context.swapContent(viewController);
+			assertFalse("NullPointerException not thrown", true);
+		} catch(Exception e) {
+			assertEquals(NullPointerException.class, e.getClass());
+			assertEquals("StageContext mainNode and content must not be null", e.getMessage());
+		}		
+	}
+	
+	@Test
+	public void swapContent()
+	{
+		StageContext context = new StageContext("test", "test title") {
+			
+			private AnchorPane pane;
+			
+			@Override
+			protected void onStart() {}
+			
+			@Override
+			protected void onCreate(URL location, ResourceBundle resources) {}
+			
+			@Override
+			public void setViewStageTitle(String title) {}
+			
+			@Override
+			public AnchorPane getMainNode() 
+			{
+				if(pane == null) {
+					pane = new AnchorPane();
+					pane.setId("stage-main-node");	
+				}
+				return pane;
+			}
+		};
+		ViewContext viewController = Mockito.mock(ViewContext.class);
+		FlowPane pane = new FlowPane();
+		pane.setId("view-main-node");
+		Mockito.when(viewController.getMainNode()).thenReturn(pane);
+		context.swapContent(viewController);
+		assertNotNull("viewController main node not found", context.getMainNode().lookup("#view-main-node"));
+		Mockito.verify(viewController, Mockito.times(1)).swapAnimation(ArgumentMatchers.any());
+		pane.setVisible(false);
+		pane.setMaxSize(FlowPane.USE_COMPUTED_SIZE, FlowPane.USE_COMPUTED_SIZE);
+		pane.setPrefSize(250, 250);
+		context.swapContent(viewController);
+		assertEquals(1, context.getMainNode().getChildren().size());
+		assertEquals(0.0, pane.getProperties().get("pane-top-anchor"));
+		assertEquals(0.0, pane.getProperties().get("pane-left-anchor"));
+		assertEquals(0.0, pane.getProperties().get("pane-bottom-anchor"));
+		assertEquals(0.0, pane.getProperties().get("pane-right-anchor"));
+		assertTrue("viewController main node still not visible", pane.isVisible());
+		assertEquals(250.0, pane.getPrefWidth(), 0.0);
+		assertEquals(250.0, pane.getPrefHeight(), 0.0);
+	}
+}
