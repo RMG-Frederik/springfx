548872dc69bd97b029e26a51b6397b417c847c23
lucasmarotta <lucas.marotta@codecrafting.net>
2018-03-09 17:21:54 -0300

    Updated project structure and added licences

--- springfx-demo/src/main/java/net/codecrafting/springfx/demo/App.java
+++ springfx-demo/src/main/java/net/codecrafting/springfx/demo/App.java
@@ -1,9 +1,26 @@
 package net.codecrafting.springfx.demo;
 
-public class App
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.codecrafting.springfx.context.ViewStage;
+import net.codecrafting.springfx.core.SpringFXApplication;
+import net.codecrafting.springfx.core.SpringFXLauncher;
+
+@SpringBootApplication
+public class App extends SpringFXApplication
 {
     public static void main(String[] args)
 	{
-        
+        try {
+			SpringFXLauncher.launch(App.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
+
+	@Override
+	public void start(ViewStage viewStage) throws Exception 
+	{
+		
+	}
 }
